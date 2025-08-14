d3c7579223cf449aead0babff1cba625940e9899
mariayotclub <marialuisaribeirobalim978@gmail.com>
2025-08-14 10:53:36 -0300

    oi

---
+++ controllers/AlunoController.js
@@ -0,0 +1,21 @@
+import aluno from '../models/alunos.js'
+
+export default class AlunoController{
+    constructor(caminhoBase='aluno/'){
+        this.caminhoBase = caminhoBase
+        this.openAdd = async (req, res)=> {
+            res.render(caminhoBase + "add")
+        }
+        this.add = async (req, res)=> {
+            await Aluno.create ({
+                nome: req.body.nome,
+                matricula: req.body.matricula
+            })
+            res.redirect('/'+caminhoBase + 'add') ;
+        }
+        this.list = async(req, res)=>{
+            const resultado = await Aluno.find({})
+            res.render(caminhoBase + 'lst',{Alunos:resultado})
+        }
+    }
+}
